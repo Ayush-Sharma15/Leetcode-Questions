c5784293d3cc69a69f2ee916ac08d2d2aac0006e
Ayush Sharma <144872617+Ayush-Sharma15@users.noreply.github.com>
2024-09-11 01:38:56 +0530

    Time: 473 ms (30.07%) | Memory: 92.2 MB (38.09%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -5,26 +5,22 @@ using namespace std;
 class Solution {
 public:
     void func(int ind, string num, long long target, vector<string>& ans, string s, long long sum, long long lastValue) {
-        // Base case: if we reach the end of the string
+        
         if (ind == num.length()) {
             if (sum == target) {
-                ans.push_back(s);  // Add valid expression to the result
+                ans.push_back(s);  
             }
             return;
         }
 
-        // Try all possible splits
         for (int i = ind; i < num.length(); i++) {
-            // Avoid numbers with leading zero
             if (i > ind && num[ind] == '0') break;
 
-            // Current number in string format
             string currentNumStr = num.substr(ind, i - ind + 1);
             long long currentNum = stoll(currentNumStr);
 
-            // If this is the first number, we simply recurse
             if (ind == 0) {
-                func(i + 1, num, target, ans, currentNumStr, currentNum, currentNum); // No operator before first number
+                func(i + 1, num, target, ans, currentNumStr, currentNum, currentNum);
             } else {
                 func(i + 1, num, target, ans, s + "+" + currentNumStr, sum + currentNum, currentNum);
 
