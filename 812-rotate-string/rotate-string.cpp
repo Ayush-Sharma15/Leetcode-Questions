350520ab4082dcfd86ed16c4df83c75abd3dfa83
Ayush Sharma <144872617+Ayush-Sharma15@users.noreply.github.com>
2024-09-20 02:16:23 +0530

    Time: 3 ms (47.49%) | Memory: 7.6 MB (59.13%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        string doubled = s + s; // Concatenate the string with itself
+        return doubled.find(goal) != string::npos; // Check if goal is a substring of doubled
+    }
+};
