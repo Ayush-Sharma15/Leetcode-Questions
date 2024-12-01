11a5b46607b6788d715ca6b49acff144babb8a4d
Ayush Sharma <144872617+Ayush-Sharma15@users.noreply.github.com>
2024-12-01 17:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (15.78%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        unordered_set<int> seen;
-        for (int num : arr) {
-            if (seen.count(num * 2) || (num % 2 == 0 && seen.count(num / 2))) {
-                return true;
-            }
-            seen.insert(num);
-        }
-        return false;
+        unordered_set<int> st;
+        for(int i=0;i<arr.size();i++)
+{
+    if(st.count(arr[i]*2) or  (arr[i]%2==0 and st.count(arr[i]/2)))
+    return true;
+    else{
+        st.insert(arr[i]);
     }
+}
+return false;    
+}
 };
