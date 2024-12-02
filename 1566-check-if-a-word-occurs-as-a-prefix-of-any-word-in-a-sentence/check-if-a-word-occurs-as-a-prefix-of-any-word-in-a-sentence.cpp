d89099637530147d65391114e11c94942ba502d6
Ayush Sharma <144872617+Ayush-Sharma15@users.noreply.github.com>
2024-12-02 15:53:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (6.42%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        int count = 0, j = 0, k = 0;
-        for (int i = 0; i < sentence.length(); i++) {
-            if (sentence[i] == searchWord[j]) {
-                j++;
-                if (j == searchWord.length() && k == 0) {
-                    return count + 1;
-                }
-                continue;
+        int count = 0; 
+        int n = searchWord.length(); 
+        stringstream ss(sentence);
+        string word;
+        
+        while (ss >> word) {
+            count++;
+            if (word.substr(0, n) == searchWord) {
+                return count;
             }
-            if (sentence[i] == ' ') {
-                count++;
-                k = 0;
-            } else {
-                k++;
-            }
-            j = 0;
         }
+        
         return -1;
     }
 };
