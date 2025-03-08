aefac37d872115a1dd0c140f8f4d1b136939b03e
Ayush Sharma <144872617+Ayush-Sharma15@users.noreply.github.com>
2025-03-08 16:33:38 +0530

    Time: 1176 ms (17.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select a.id
+from Weather as a
+inner join Weather as b
+where datediff (a.recordDate, b.recordDate) =1 and a.temperature > b.temperature;
+
